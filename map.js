e77ff4cc4ee1d9316153c00c7d3a03b5aa7a2bf3
Chris Humphries <chris.humphries1@me.com>
2013-03-07 13:00:56 -0600

    Initial Commit

---
+++ map.js
@@ -0,0 +1,49 @@
+ <script type="text/javascript">
+        $(function() {
+                // Also works with: var yourStartLatLng = '59.3426606750, 18.0736160278';
+                var yourStartLatLng = new google.maps.LatLng(59.3426606750, 18.0736160278);
+                $('#map_canvas').gmap({'center': yourStartLatLng});
+                
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.62258872997,-86.599988937378', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.621230761301,-86.567373275757', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.605237283535,-86.548898220062', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.546366469076,-86.613292694092', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.514635035823,-86.663246154785', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.485030599532,-86.702020168304', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.431763668305,-86.713886260986', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.387104709722,-86.778066158295', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.363344,-86.813822', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.310329790262,-86.870672106743', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.297244,-86.843658', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.185139500547,-87.001166939735', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.169707677776,-87.017211914062', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.165553,-87.029444', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.159892,-87.025933', 'bounds': true } );
+                 $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.094993107768,-87.057638168335', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.080236,-87.066994', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.077236,-87.072814', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '33.018681,-87.078219', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '32.944942,-87.139044', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '32.855652799757,-87.19792842865', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '32.774700327269,-87.272965908051', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '32.694539,-87.243008', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '32.668226600493,-87.24228143692', 'bounds': true } );
+                 $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '32.591623528431,-87.217326164246', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '32.528578828718,-87.203464508057', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '32.44445036694,-87.180236577988', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '32.35945,-87.133056', 'bounds': true } );
+                $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '32.315653178625,-87.09743142128', 'bounds': true } );
+                
+                 $('#map_canvas').gmap('addMarker', { 'position':  '32.315653178625,-87.09743142128', 'bounds': true }).click(function() {
+                $('#map_canvas').gmap('openInfoWindow', { 'content': 'Take-Out' }, this);
+        });
+       
+
+        });
+        
+       
+                                                                                                                                                                                                                                    
+
+        
+        
+</script>
